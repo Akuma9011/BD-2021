1ad63d7e2cdc65fc43287b63be9d028ec9089749
Akuma9011 <94305786+Akuma9011@users.noreply.github.com>
2021-11-14 13:45:02 -0500

    tercer comit
    tercer commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./prueba2021-2ae4d-75b8bfe6fddd.json');
+const serviceAccount = require('../backend/prueba2021-2ae4d-75b8bfe6fddd.json');
 
 initializeApp({
     credential: cert(serviceAccount)
