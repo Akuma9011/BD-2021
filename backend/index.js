754da0a5b72cf2afa06c9f08a3f0dab0b96b1ae5
Akuma9011 <94305786+Akuma9011@users.noreply.github.com>
2021-11-15 22:49:45 -0500

    crud realizado basico
    Se hace el crud para expertos

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,26 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
-
 const dbE = require('./src/db/crudExperts.js');
+const db = require('./src/db/firebase.js');
 
-app.use(express.static('public'));
 
+// Para paginas estaticas .html
+//app.use(express.static('public'));
+
+//Para poder usar json en peticiones
+app.use(express.json());
+
+
+// Mensaje Hola en la pagina principal
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Bienvnido')
   })
+// Se activa el servidor en el perto 3000
 app.listen(3000)
 
 
+//Traer tods los expertos
 app.get('/get-experts', function(req, res){
     dbE.getExperts(function(arrayExperts){
         res.send(arrayExperts);
@@ -18,6 +28,50 @@ app.get('/get-experts', function(req, res){
 })
 
 
+
+//Traer un solo experto
+app.get("/get-expert/:id", function(req,res){
+    const eid = req.params.id;
+    dbE.getExpert(eid,function(expert){
+        res.json(expert);
+    })
+})
+
+
+
+// Agregar un experto
+app.post("/add-expert",(req, res)=>{
+    const expert = req.body;
+    dbE.addExpert(expert, function(response){
+        res.send(response);
+    })
+})
+
+//Actualizar un experto
+app.put("/update-expert-totally/:id", (req, res)=>{
+    const eid = req.params.id;
+    const expert = req.body;
+    dbE.updateExpertTotally(eid, expert, function(response){
+        res.send(response);
+    })
+})
+
+//Actualizado parcialmente
+app.patch("/update-partial/:id", (req, res)=>{
+    const eid = req.params.id;
+    const expert = req.body;
+    dbE.updateExpertPartial(eid, expert, (response)=>{
+        res.send(response);
+    })
+})
+
+//Borradode experto
+app.delete('/delete-expert/:id',(req, res)=>{
+    const eid = req.params.id;
+    dbE.deleteExpert(eid, (response)=>{
+        res.send(response);
+    })
+})
 
 
 
--- backend/src/db/crudExperts.js
+++ backend/src/db/crudExperts.js
@@ -1,19 +1,8 @@
+const db = require('./firebase.js');
 const dbE = require('./firebase.js');
 
-/* // Trae todos los usuarios de la coleccion experts
-function getExperts() {
-    return dbE.collection('experts').get()
-        .then((refDoc) => {
-            refDoc.forEach((doc)=>{
-                console.log(doc.id, '=>', doc.data());
-            })
-        }) 
-        .catch(err =>{
-                console.error("Error al traer los datos");
-            })
-}
- */
 
+//Trae todos los expertos
 function getExperts(callback) {
     return dbE.collection('experts').get()
         .then((refDoc) => {
@@ -31,6 +20,91 @@ function getExperts(callback) {
 }
 
 
-module.exports = {
-    getExperts
+//Trae un experto
+function getExpert(eid, callback){
+    return db.collection('experts').doc(eid).get()
+    .then((doc)=>{
+        callback(doc.data());
+    })
+    .catch((err=>{
+       callback("Ha ocurrio un error");
+    }))
 }
+
+//Agrega un experto
+function addExpert(expert,callback){
+    return db.collection('experts').add(expert)
+    .then(()=>{
+        callback("Expert created...!!");
+    })
+    .catch((error)=>{
+        callback('Error to add expert...!!!');
+    })
+}
+
+//Actualizar un experto
+function updateExpertTotally(eid, expert, callback){
+    return dbE.collection('experts').doc(eid).set(expert)
+    .then(()=>{
+        callback('Success');
+    })
+    .catch((err)=>{
+        callback("Fallo al actualizar");
+    })
+}
+
+
+//Actualizar experto parcialmente
+function updateExpertPartial(eid, expert, callback){
+    return dbE.collection('experts').doc(eid).update(expert)
+    .then(()=>{
+        callback('Actualizado parcialmente');
+    })
+    .catch((err)=>{
+        callback('Hubo un error en la actualizacion');
+    })
+}
+
+
+//Borrar un experto
+function deleteExpert(eid, callback){
+    return dbE.collection('experts').doc(eid).delete()
+    .then(()=>{
+        callback("Borrado exitoso");
+    })
+    .catch((err)=>{
+        callback('Error al borrar')
+    })
+
+}
+
+
+
+
+
+
+
+/* // Trae todos los usuarios de la coleccion experts
+function getExperts() {
+    return dbE.collection('experts').get()
+        .then((refDoc) => {
+            refDoc.forEach((doc)=>{
+                console.log(doc.id, '=>', doc.data());
+            })
+        }) 
+        .catch(err =>{
+                console.error("Error al traer los datos");
+            })
+}
+ */
+
+
+module.exports = {
+    getExperts,
+    getExpert,
+    addExpert,
+    updateExpertTotally,
+    updateExpertPartial,
+    deleteExpert
+}
+
