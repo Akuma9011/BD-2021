64810127c826fcdf48cccdef5d0c7773daf1ee28
Akuma9011 <94305786+Akuma9011@users.noreply.github.com>
2021-11-16 00:00:46 -0500

    CRUD sin rutas
    Crusd sin rutas

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const dbE = require('./src/db/crudExperts.js');
 const db = require('./src/db/firebase.js');
+//const axios = require('axios').default;
+const axios = require('axios');
 
 
 // Para paginas estaticas .html
@@ -15,24 +17,24 @@ app.use(express.json());
 // Mensaje Hola en la pagina principal
 app.get('/', function (req, res) {
     res.send('Bienvnido')
-  })
+})
 // Se activa el servidor en el perto 3000
 app.listen(3000)
 
 
 //Traer tods los expertos
-app.get('/get-experts', function(req, res){
-    dbE.getExperts(function(arrayExperts){
+app.get('/get-experts', function (req, res) {
+    dbE.getExperts(function (arrayExperts) {
         res.send(arrayExperts);
-    })    
+    })
 })
 
 
 
 //Traer un solo experto
-app.get("/get-expert/:id", function(req,res){
+app.get("/get-expert/:id", function (req, res) {
     const eid = req.params.id;
-    dbE.getExpert(eid,function(expert){
+    dbE.getExpert(eid, function (expert) {
         res.json(expert);
     })
 })
@@ -40,37 +42,58 @@ app.get("/get-expert/:id", function(req,res){
 
 
 // Agregar un experto
-app.post("/add-expert",(req, res)=>{
+app.post("/add-expert", (req, res) => {
     const expert = req.body;
-    dbE.addExpert(expert, function(response){
+    dbE.addExpert(expert, function (response) {
         res.send(response);
     })
 })
 
 //Actualizar un experto
-app.put("/update-expert-totally/:id", (req, res)=>{
+app.put("/update-expert-totally/:id", (req, res) => {
     const eid = req.params.id;
     const expert = req.body;
-    dbE.updateExpertTotally(eid, expert, function(response){
+    dbE.updateExpertTotally(eid, expert, function (response) {
         res.send(response);
     })
 })
 
 //Actualizado parcialmente
-app.patch("/update-partial/:id", (req, res)=>{
+app.patch("/update-partial/:id", (req, res) => {
     const eid = req.params.id;
     const expert = req.body;
-    dbE.updateExpertPartial(eid, expert, (response)=>{
+    dbE.updateExpertPartial(eid, expert, (response) => {
         res.send(response);
     })
 })
 
 //Borradode experto
-app.delete('/delete-expert/:id',(req, res)=>{
+app.delete('/delete-expert/:id', (req, res) => {
     const eid = req.params.id;
-    dbE.deleteExpert(eid, (response)=>{
+    dbE.deleteExpert(eid, (response) => {
         res.send(response);
     })
+})
+
+
+
+//Traer paises
+app.post("/experts-country", (req, res) => {
+    const expert = req.body;
+    const country = expert.location;
+
+    axios.get('https://restcountries.com/v3.1/name/' + country)
+        .then(function (response) {
+            expert.language = response.data[0].languages;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            dbE.addExpert(expert, function (response) {
+                res.send(response);
+            });
+        })
 })
 
 
